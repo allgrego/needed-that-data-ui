66f8377dccfb24a2101d1a9426b037c32fee0fff
Gregorio Alvarez <allgrego14@gmail.com>
2023-06-09 19:08:39 -0400

    [task | style] Minor changes in error message and file styling format

--- src/common/utils/fetch.ts
+++ src/common/utils/fetch.ts
@@ -1,5 +1,6 @@
 export const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
 
-if (!BACKEND_BASE_URL) throw new Error("Backend URL is not defined");
+if (!BACKEND_BASE_URL)
+  throw new Error("BACKEND_BASE_URL is not set as environment variable");
 
 export const BACKEND_API_KEY = process.env.BACKEND_API_KEY;
